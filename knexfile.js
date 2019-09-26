3d8601e7959448dc74641474f8c62574f5f7de5b
igorOrtega <48002130+igorOrtega@users.noreply.github.com>
2019-09-26 18:55:46 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,9 @@
 module.exports = {
 	client: 'postgresql',
 	connection: {
-	database: 'projeto_confere',
-	user:     'postgres',
-	password: '123456'
+	database: 'd4trc2h3t3s3dn',
+	user:     'jzldvetppvdrgj',
+	password: '6cd9a9d84b48e24fd15e8c2c09fe89bcbf105a6cb991b9dfddb1d32672bb65a7'
 	},
 	pool: {
 	min: 2,
