7be195d14e8247f00a9478bf9e399643e746ff1a
igorOrtega <48002130+igorOrtega@users.noreply.github.com>
2019-09-26 20:02:46 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,11 @@
 
 module.exports = {
 	client: 'postgresql',
-	connection: process.env.DATABASE_URL,
+	connection: process.env.DATABASE_URL || {
+		database: 'projeto_confere',
+		user:     'postgres',
+		password: '123456'
+	},
 	migrations: {
 	tableName: 'knex_migrations'
 	},
